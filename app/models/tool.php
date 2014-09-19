2f4cfbc91e4ffcc334e56e7a39cd69fc4200fa45
Martin Polak <brko11@gmail.com>
2014-09-19 10:20:48 +0200

    uprava stahovani kurzu CNB (hosting nepodporuje file_get_contents ale curl)

--- app/models/tool.php
+++ app/models/tool.php
@@ -72,7 +72,13 @@ class Tool extends AppModel {
 	}
 	
 	function download_url($url) {
-		$data = file_get_contents($url);
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+		$data = curl_exec($ch);
+		curl_close($ch);
 		return $data;
 	}
 	
